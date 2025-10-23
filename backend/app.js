0a6f2926bf5fc0fe8eeb66f897e92324fa0fccce
Gaurav Kumar Jha <67746431+gauravjha-hackerboy@users.noreply.github.com>
2025-10-23 16:03:51 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
-app.get('/auth/login', (req, res) => {
-  res.json({ message: 'Login endpoint is working. Use POST to login.' });
+app.get('/', (res) => {
+  res.json({ message: 'welcome to backend' });
 });
 mongoose
   .connect(process.env.MONGO_URI, {
